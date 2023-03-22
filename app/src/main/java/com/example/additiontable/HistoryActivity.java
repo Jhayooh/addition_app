5257c535ad045da924cf0f20352266b87c675446
Jhayooh <macalinaojhay9@gmail.com>
2023-03-22 20:20:32 +0800

    mostly history activity changes.com

--- app/src/main/java/com/example/additiontable/HistoryActivity.java
+++ app/src/main/java/com/example/additiontable/HistoryActivity.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Summary extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_summary);
+        setContentView(R.layout.activity_history);
     }
-
-    public void launchMainActivity(View view){
-        Intent intent = new Intent(this, MainActivity.class);
+    public void backButton(View view) {
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
+    }
+    public void launchSummary (View view){
+        Intent intent = new Intent(this, Summary.class);
         startActivity(intent);
         finish();
     }
