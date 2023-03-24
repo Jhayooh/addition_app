d30eaa5a2acd3dc609474b092e07cb8df8f239d9
Jhayooh <macalinaojhay9@gmail.com>
2023-03-24 21:30:08 +0800

    history activity

--- app/src/main/java/com/example/additiontable/HistoryActivity.java
+++ app/src/main/java/com/example/additiontable/HistoryActivity.java
@@ -1,10 +1,17 @@
 package com.example.additiontable;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -18,7 +25,13 @@ public class HistoryActivity extends AppCompatActivity {
         finish();
     }
     public void launchSummary (View view){
-        Intent intent = new Intent(this, Summary.class);
+        Intent intent = new Intent(HistoryActivity.this, Summary.class);
+        LinearLayout linear = findViewById(view.getId());
+        View child = linear.getChildAt(1);
+        TextView textView = findViewById(child.getId());
+        String p_name = textView.getText().toString();
+        intent.putExtra(Summary.sum_pName, p_name);
+        intent.putExtra("isHistory", true);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/additiontable/Summary.java
+++ app/src/main/java/com/example/additiontable/Summary.java
@@ -1,10 +1,13 @@
 package com.example.additiontable;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -14,6 +17,7 @@ public class Summary extends AppCompatActivity {
     public static final String sum_pName = "name";
     public static final String sum_tile = "tile";
     public static final String sum_time = "time";
+    public static Boolean isHistory=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,15 @@ public class Summary extends AppCompatActivity {
         text_name.setText(getIntent().getStringExtra(sum_pName));
         text_tile.setText(getIntent().getStringExtra(sum_tile));
         text_time.setText(getIntent().getStringExtra(sum_time));
+        Bundle bundle = getIntent().getExtras();
+        boolean isHistory = bundle.getBoolean("isHistory");
+        if (isHistory) {
+            Button btn = findViewById(R.id.play_again);
+            ImageView img = findViewById(R.id.back_button);
+            btn.setVisibility(View.GONE);
+            img.setVisibility(View.VISIBLE);
+            isHistory=false;
+        }
     }
 
     public void launchMainActivity(View view){
@@ -32,5 +45,9 @@ public class Summary extends AppCompatActivity {
         intent.putExtra(MainActivity.s_tile, getIntent().getStringExtra(sum_tile));
         startActivity(intent);
         finish();
+    }
+
+    public void backButtonSum(View view) {
+        startActivity(new Intent(Summary.this, HistoryActivity.class)); finish();
     }
 }
